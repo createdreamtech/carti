c76917b81f965e8953df9789de4fa2528d21200a
Zane Starr <zcstarr@gmail.com>
2020-12-17 16:19:29 -0800

    fix: temporarily remove support for --version

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -5,14 +5,13 @@ import { makeLogger } from "../lib/logging";
 import { startCartiFromCLI } from "./commands";
 import _ from "lodash";
 import { addRepoCommand } from "./commands/repo";
-import { Repo } from "../lib/repo";
 import { startCarti } from "..";
 import { addBundleCommand } from "./commands/bundle";
 
 const logger = makeLogger("cli");
 const config = startCarti()
 program
-  .version(version, "-v, --version")
+  //.version(version, "-v, --version")
   .addCommand(addRepoCommand(config.repo))
   .addCommand(addBundleCommand())
   .parse(process.argv);
