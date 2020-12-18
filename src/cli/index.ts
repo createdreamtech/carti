824598c93d8b091357b2958818b7bba3a7594637
Zane Starr <zcstarr@gmail.com>
2020-12-17 16:12:25 -0800

    feat: add bundle command to cli

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -7,10 +7,12 @@ import _ from "lodash";
 import { addRepoCommand } from "./commands/repo";
 import { Repo } from "../lib/repo";
 import { startCarti } from "..";
+import { addBundleCommand } from "./commands/bundle";
 
 const logger = makeLogger("cli");
 const config = startCarti()
 program
   .version(version, "-v, --version")
   .addCommand(addRepoCommand(config.repo))
+  .addCommand(addBundleCommand())
   .parse(process.argv);
