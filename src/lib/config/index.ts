221492e87ca14b1e367caf7d241835d8da4d6c8e
Zane Starr <zcstarr@gmail.com>
2020-12-21 15:26:21 -0800

    feat: refactor config to include repo

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -2,11 +2,14 @@ import process from "process"
 import os from "os"
 import { CartiConfigStorage } from "../storage"
 import { CartiBundleStorage } from "../storage/carti_bundles"
+import { Repo } from "../repo"
+import { fetcher } from "../fetcher";
 
 export interface Config {
     localConfigStorage: CartiConfigStorage 
     globalConfigStorage: CartiConfigStorage 
     bundleStorage: CartiBundleStorage
+    repo: Repo
 }
 const bundlesPath = `${process.cwd()}/carti_bundles`
 const bundleListingFilename = ".carti_bundles.json"
@@ -16,9 +19,11 @@ const localBundleListingPath =`${bundlesPath}/.carti`
 const localConfigStorage = new CartiConfigStorage(localBundleListingPath, bundleListingFilename)
 const globalConfigStorage = new CartiConfigStorage(globalBundleListingPath, bundleListingFilename)
 const bundleStorage = new CartiBundleStorage(bundlesPath)
+const repo = new Repo(globalConfigStorage,fetcher) 
 
 export const config:Config = {
     localConfigStorage,
     globalConfigStorage,
-    bundleStorage
+    bundleStorage,
+    repo
 }
