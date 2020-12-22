777cdf1fefa22d2ef67858302d084e9af8d05b04
Zane Starr <zcstarr@gmail.com>
2020-12-21 20:01:08 -0800

    feat: add support for optional bundle prompt questions for cli

---
+++ src/cli/util.ts
@@ -0,0 +1,18 @@
+import { Bundle } from "@createdreamtech/carti-lib"
+import inquirer, { ListQuestion } from "inquirer"
+export interface BundleRenderer {
+    (b: Bundle): string
+}
+export const defaultBundleRenderer = (b: Bundle)=>{
+    return `${b.name} type:${b.bundleType} version:${b.version} uri:${b.uri}`
+}
+export function pickBundle(message: string, bundles: Array<Bundle>, renderer: BundleRenderer): ListQuestion {
+    const choices = bundles.map(renderer)
+    return {
+        name: "bundle",
+        message,
+        type: "list",
+        choices
+    }
+
+}
