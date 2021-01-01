61409873f2274d3486b2c6c45017d7665e7388a6
Zane Starr <zcstarr@gmail.com>
2021-01-01 15:47:27 -0800

    feat: add support for installation

--- src/cli/commands/install.ts
+++ src/cli/commands/install.ts
@@ -1,11 +1,34 @@
+import { Bundle, makeHttpFetcher } from "@createdreamtech/carti-lib";
 import program from "commander";
+import { parseShortDesc, shortDesc } from "../../lib/bundle";
+import { Config } from "../../lib/config";
+import inquirer from "inquirer";
+import * as utils from "../util";
+import { bundle } from "@createdreamtech/carti-lib";
+import { bundleFetcher } from "../../lib/fetcher";
+
 interface InstallCommand {
     name: string
 }
-export const fetchCommand = (handler: (c: any)=>Promise<void>): program.Command => {
+export const addInstallCommand = (config: Config): program.Command => {
    return program
-   .command("fetch <name>") 
-   .description("Retrieve a carti bundle from a repo")
-   .action(handler)
+   .command("install <name>") 
+   .description("Install package locally")
+   .action(async (name)=>{
+       return handleInstall(config,name)
+   })
 }
 
+const renderBundle = (b: Bundle): string => {
+    const {bundleType, name, version, id, uri} = b;
+    return shortDesc({ bundleType, name, version, id , uri}) 
+}
+
+async function handleInstall(config: Config, name:string): Promise<void> {
+    const bundles = await config.globalConfigStorage.get(name)
+    const question = utils.pickBundle("Which bundle would you like to install", bundles, renderBundle)
+    const answer = await inquirer.prompt([question])
+    const {id} = parseShortDesc(answer.bundle)
+    const bun = bundles.filter((b) => b.id === id)[0]
+    await bundle.install(bun,bundleFetcher(bun.uri as string), config.bundleStorage)
+}
