d5a5998a1198f05cbfa1b23ae014b8ccdfa173de
Zane Starr <zcstarr@gmail.com>
2021-01-01 15:51:30 -0800

    feat: expose commands to cli

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,12 +2,14 @@
 import program from "commander";
 const version = require("../../../package.json").version; // tslint:disable-line
 import { makeLogger } from "../lib/logging";
-import { startCartiFromCLI } from "./commands";
 import _ from "lodash";
 import { addRepoCommand } from "./commands/repo";
 import { addBundleCommand } from "./commands/bundle";
 import { addPublishCommand } from "./commands/publish";
 import {config} from "../lib/config"
+import { addInstallCommand } from "./commands/install";
+import { addMachineCommand } from "./commands/machine";
+import { addListCommand } from "./commands/list";
 
 const logger = makeLogger("cli");
 program
@@ -15,4 +17,7 @@ program
   .addCommand(addRepoCommand(config.repo))
   .addCommand(addBundleCommand())
   .addCommand(addPublishCommand(config))
+  .addCommand(addInstallCommand(config))
+  .addCommand(addMachineCommand(config))
+  .addCommand(addListCommand(config))
   .parse(process.argv);
