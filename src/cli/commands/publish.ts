c5db4219fbb0a49957e3bbcaec20f8c177c1e90e
Zane Starr <zcstarr@gmail.com>
2021-01-01 15:49:00 -0800

    fix: refactor support for publishing bundles

--- src/cli/commands/publish.ts
+++ src/cli/commands/publish.ts
@@ -1,10 +1,11 @@
 import program, { commands } from "commander";
 import { makeLogger } from "../../lib/logging"
 import * as clib  from "@createdreamtech/carti-lib"
-import { Bundle, Storage, bundle,  DiskProvider } from "@createdreamtech/carti-lib"
+import { Bundle, Storage, bundle,  DiskProvider, S3Provider } from "@createdreamtech/carti-lib"
 import {Config} from "../../lib/config"
 import inquirer, {Question} from "inquirer"
 import * as utils from "../util"
+import path from "path";
 import {shortDesc, parseShortDesc } from "../../lib/bundle";
 
 interface PublishOptions {
@@ -23,42 +24,40 @@ export const publishCommand = (handler: (c: any)=>Promise<void>): program.Comman
 export const addPublishCommand = (config: Config): program.Command => {
    const publishCommand = new program.Command("publish")
       .description("Publish carti bundle to permanent storage")
-   publishCommand.command("s3 <src> <uri>")
-      .description("add bundle to s3 listing repo")
-      .option("--nosave", "don't add to .bundles.json")
-      .action((src, uri)=>{
-          handlePublish(config, src, new Storage(new DiskProvider("/tmp")))
+   publishCommand.command("s3 <bundle> <bucket> <uri>")
+      .description("add bundle to s3 listing repo bundle ")
+      .usage("s3 bundleName bucketName publicURI")
+      .option("--nosave", "don't add to bundles.json")
+      .requiredOption("--bucket", "Name of the s3 bucket to upload to")
+      .action((bundle, bucket, uri, options)=>{
+          handlePublish(config, bundle, new Storage(new S3Provider(bucket)), uri)
       })
    publishCommand.command("disk <src> <path>")
       .description("Publish file to disk storage for testing")
-      .option("--nosave", "don't add to .bundles.json")
-      .action((src, path, options)=>{
-          handlePublish(config, src, new Storage(new DiskProvider(path)))
+      .option("--nosave", "don't add to bundles.json")
+      .action((src, pth, options)=>{
+          const absPath =path.resolve(pth)
+          handlePublish(config, src, new Storage(new DiskProvider(absPath)), absPath)
       })
    return publishCommand
 }
 
-const publishQuestions: Question[] = [
-    {
-        type: "list",
-        name: "bundle",
-        message: "Which bundle would you like to publish?"
-    }
-]
+
 const renderBundle = (b: Bundle): string => {
     const {bundleType, name, version, id} = b;
     return shortDesc({ bundleType, name, version, id , uri: "local"}) 
 }
 
-async function handlePublish(config: Config, name:string, storage: Storage): Promise<void> {
+async function handlePublish(config: Config, name:string, storage: Storage, uri?:string): Promise<void> {
     const bundles: Bundle[] = await config.localConfigStorage.get(name)
-    console.log(bundles)
     const question = utils.pickBundle("Which bundle would you like to publish", bundles, renderBundle)
     const answer = await inquirer.prompt([question])
     const {id} = parseShortDesc(answer.bundle)
     const bundle = bundles.filter((b) => b.id === id)[0]
-    const uploadBundle = Object.assign({}, bundle, { path: bundle.uri as string }) 
-    delete uploadBundle.uri
-    console.log(uploadBundle)
-    clib.bundle.bundle(uploadBundle as bundle.BundleMeta, storage)
+    const uploadBundle = Object.assign({}, bundle, { path: bundle.uri as string, uri }) 
+    if(!uri)
+        delete uploadBundle.uri
+    const bun = await clib.bundle.bundle(uploadBundle as bundle.BundleMeta, storage)
+    bun.uri = uri
+    config.bundleListingManager.addBundle(bun)
 }
