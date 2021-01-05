d7e4027b444baf2e0a71df1e57aac23872bc4c1d
Zane Starr <zcstarr@gmail.com>
2021-01-05 13:32:11 -0800

    feat: add support for listing all packages local or not installed

--- src/cli/commands/list.ts
+++ src/cli/commands/list.ts
@@ -3,29 +3,37 @@ import { makeLogger } from "../../lib/logging"
 import { Bundle } from "@createdreamtech/carti-core"
 import { Config } from "../../lib/config"
 import { shortDesc } from "../../lib/bundle";
+import { Listing } from "../../lib/storage/bundle_listing";
 
 
 export const addListCommand = (config: Config): program.Command => {
     const machineCommand = program.command("list")
-        .description("Lists all the installed packages")
+        .description("Lists installed bundles see --all for all bundles")
         .usage("list")
-        .action(async () => {
-            return handleList(config)
+        .option("--all", "lists all bundles installed or retrievable")
+        .action(async (options:any) => {
+            return handleList(config,options.all)
         })
     return machineCommand
 }
 
 
-const renderBundle = (b: Bundle): string => {
+const renderBundle = (b: Bundle, uri: string): string => {
     const { bundleType, name, version, id } = b;
-    return shortDesc({ bundleType, name, version, id, uri: "local" })
+    return shortDesc({ bundleType, name, version, id, uri })
 }
 
-async function handleList(config: Config): Promise<void> {
-    const list = await config.localConfigStorage.listing()
-    let localBundleDescs: string[] = []
-    Object.keys(list).forEach((key: string) => {
-        localBundleDescs = localBundleDescs.concat(list[key].map((b: Bundle) => renderBundle(b)))
-    })
-    console.log(localBundleDescs.join("\r\n"))
+async function handleList(config: Config, all:boolean): Promise<void> {
+    let localList = await config.localConfigStorage.listing();
+    let globalList:Listing = {}; 
+    if(all)
+        globalList = await config.globalConfigStorage.listing()
+    const renderList = (list: Listing, uriType?: string): string[] => {
+        let descs: string[] = []
+        Object.keys(list).forEach((key: string) => {
+            descs = descs.concat(list[key].map((b: Bundle) => renderBundle(b, uriType || b.uri || 'global')))
+        })
+        return descs
+    }
+    console.log(renderList(localList,'local').concat(renderList(globalList)).join('\r\n'))
 }
