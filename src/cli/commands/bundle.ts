128ee4e15b1e76c4fa44e8a3591d61f06253ff2f
Zane Starr <zcstarr@gmail.com>
2021-01-16 08:21:55 -0800

    feat: add support for uri resolved bundling bundle ... https://* fixes #16

--- src/cli/commands/bundle.ts
+++ src/cli/commands/bundle.ts
@@ -4,6 +4,9 @@ import * as cartiLib from "@createdreamtech/carti-core"
 import path, { basename } from "path";
 import { CID } from "multiformats"
 import { config } from "../../lib/config"
+import fs from "fs-extra";
+import os from "os";
+import https from "https"
 
 const bundler = cartiLib.bundle
 const logger = makeLogger("Bundle Command")
@@ -15,11 +18,71 @@ interface BundleCommand {
     name: string
     desc: string
     version: string
+    filename?: string
 }
 
-const handleBundleCommand = async (bundlePath: string, bundle: BundleCommand) => {
-    const { name, type, desc, version } = bundle;
+const isFilePath = (filePath: string): boolean => {
+    return fs.pathExistsSync(filePath)
+}
+
+const downloadAsset = async (uri: string, fileName?: string): Promise<string> => {
+    const pt = await fs.mkdtemp(`${os.tmpdir()}/carti-tmp`)
+
+    const parseFilename = (contentDisposition?: string): string | null => {
+        if (!contentDisposition)
+            return ""
+        const match = contentDisposition.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/)
+        return match ? match[1] : ""
+    }
+    return new Promise((resolve, reject) => {
+
+        let downloadFile!: fs.WriteStream;
+        let fullPath: string;
+        https.get(uri, async (response) => {
+
+            if (response.statusCode == 302) {
+                fullPath = await downloadAsset(response.headers['location'] as string, fileName);
+            }
+            else if (response.statusCode !== 200) {
+                console.log(response.statusCode)
+                reject(new Error(`Failed to get '${uri}' (${response.statusCode})`));
+                return;
+            }
+            
+            response.headers['']
+            const f = fileName || parseFilename(response.headers['content-disposition'])
+            if (f === "") {
+                throw new Error("please set a filename could not parse filename from uri")
+            }
+            fullPath = `${pt}/${f}`
+            downloadFile = fs.createWriteStream(fullPath);
+            downloadFile.on('finish', () => resolve(fullPath));
+
+        /*    request.on('error', err => {
+                fs.unlink(fullPath, () => reject(err));
+            });
+            */
+
+            downloadFile.on('error', err => {
+                fs.unlink(fullPath, () => reject(err));
+            });
+
+            response.pipe(downloadFile);
+
+        });
+
+    });
+}
+
+const handleBundleCommand = async (bundleUri: string, bundle: BundleCommand) => {
+    const { name, type, desc, version, filename } = bundle;
     const { bundleStorage, localConfigStorage } = config
+    let bundlePath = bundleUri
+    let shouldClean = false;
+    if (isFilePath(bundleUri) === false) {
+        bundlePath = await downloadAsset(bundleUri, filename)
+        shouldClean = true;
+    }
     const bun = await bundler.bundle({
         bundleType: type,
         name,
@@ -27,6 +90,9 @@ const handleBundleCommand = async (bundlePath: string, bundle: BundleCommand) =>
         path: path.resolve(bundlePath),
         version,
     }, bundleStorage)
+    if(shouldClean){
+        await fs.remove(bundlePath)
+    }
     const bPath = await bundleStorage.path(CID.parse(bun.id))
     const bundles = [Object.assign({}, bun, { uri: bPath })]
     await localConfigStorage.add(bPath, bundles)
@@ -34,13 +100,14 @@ const handleBundleCommand = async (bundlePath: string, bundle: BundleCommand) =>
 }
 
 export const addBundleCommand = (): program.Command => {
-    return program.command("bundle <path>")
-        .description("Bundle data for a cartesi machine")
+    return program.command("bundle <src>")
+        .description("Bundle data for a cartesi machine src is a url payload or filepath")
         .storeOptionsAsProperties(false)
         .passCommandToAction(false)
         .requiredOption("-t, --type <type>", "input type of data raw|ram|rom|flashdrive")
         .requiredOption("-n, --name <name>", "name of the bundle")
         .requiredOption("-v, --version <version>", "version of the bundle")
         .requiredOption("-d, --desc <desc>", "description of the bundle")
+        .option("-f, --filename [name]", "name of the bundle")
         .action(handleBundleCommand)
 }
