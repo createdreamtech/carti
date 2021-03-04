c22810b790a89e630ca399a9d498915472001ff2
Zane Starr <zcstarr@gmail.com>
2021-03-04 11:51:06 -0800

    fix: refactor integration tests

--- src/cli/index.test.ts
+++ src/cli/index.test.ts
@@ -8,61 +8,11 @@ import os from "os"
 const rmAll = promisify(rimraf)
 const version = require("../../package.json").version; // tslint:disable-line
 
-const LOCAL_BASE_TEST_DIR = fs.mkdtempSync(`${os.tmpdir()}/test-carti`)
-const LOCAL_TEST_HOME = LOCAL_BASE_TEST_DIR
-const LOCAL_TEST_PROJECT_DIR = `${LOCAL_BASE_TEST_DIR}/local-project`
+const locations = testUtil.createCartiTestLocations()
+const { localTestEnvironment, remoteTestEnvironment } = testUtil.createTestEnvironments(locations) 
 
-const REMOTE_BASE_TEST_DIR = fs.mkdtempSync(`${os.tmpdir()}/test-carti`)
-const REMOTE_TEST_HOME = REMOTE_BASE_TEST_DIR
-const REMOTE_TEST_PROJECT_DIR = `${REMOTE_BASE_TEST_DIR}/remote-project`
-// Takes project directory to build, test home directory, and then if local specified it sets the 
-// node_path to be the cwd for testing purposes
-const createTestEnvironment = (projectDir: string, testHome: string, local: boolean): testUtil.TestEnv => {
-    fs.ensureDirSync(projectDir)
-    let env: any = { HOME: testHome, PATH: process.env.PATH }
-    if (local)
-        env["NODE_PATH"] = `${process.cwd()}/node_modules`
-    return {
-        cwd: projectDir,
-        env
-    }
-}
 const CARTI_TEST_LOCAL = process.env["CARTI_TEST_LOCAL"] ? true : false;
-const localTestEnvironment = createTestEnvironment(LOCAL_TEST_PROJECT_DIR, LOCAL_TEST_HOME, CARTI_TEST_LOCAL)
-const remoteTestEnvironment = createTestEnvironment(REMOTE_TEST_PROJECT_DIR, REMOTE_TEST_HOME, CARTI_TEST_LOCAL)
-
-const contains = (phrase: string) => {
-    return (res: SpawnSyncReturns<Buffer>): boolean => {
-        if (res.error || !res.stdout)
-            throw res.error
-        const output = res.stdout.toString()
-        return output.match(phrase) !== null
-    }
-}
-const containsAsync = (phrase: string) => {
-    return (res: string): boolean => {
-        return res.match(phrase) !== null
-    }
-}
-
 const cartiCmd = (pth: string) => `${pth}/node_modules/.bin/carti`
-const setup = async (env: testUtil.TestEnv, local: boolean) => {
-    // setup environment to install itself in a clean dir
-    // set a static package directory to skip npm pack
-    if (!local) {
-        spawnSync("npm", ["pack"])
-        const cartiNodePackage = `createdreamtech-carti-${version}.tgz`
-        fs.copyFileSync(`${process.cwd()}/${cartiNodePackage}`, `${env.cwd}/${cartiNodePackage}`)
-        spawnSync("npm", ["init", "-y"], { cwd: env.cwd })
-        const res = spawnSync("npm", ["install", `${env.cwd}/${cartiNodePackage}`], { cwd: env.cwd })
-        if (res.error)
-            console.error(res.error)
-    }
-    fs.copyFileSync(`${__dirname}/../fixtures/dapp-test-data.ext2`, `${env.cwd}/dapp-test-data.ext2`)
-    const helpCommand = testUtil.createTestCommand(`${cartiCmd(env.cwd)} --help`, containsAsync("help"))
-    await testUtil.testCommand(helpCommand, env, local)
-}
-
 
 const testBundleCmdArgs = (dir: string) => {
     return `${cartiCmd(dir)} bundle -t flashdrive -n dapp-test-data -v 1.0.0 -d hello_world_flash_drive dapp-test-data.ext2`
@@ -107,7 +57,7 @@ const testAddRepoCmdArgs = (dir: string, uri: string) => {
 }
 
 const testAddRepoCommand = (dir: string, uri: string) => {
-    return testUtil.createTestCommand(testAddRepoCmdArgs(dir, uri), () => true)
+   return testUtil.createTestCommand(testAddRepoCmdArgs(dir, uri), () => true)
 }
 
 const testMachineInitCmdArgs = (dir: string) => {
@@ -158,8 +108,8 @@ const testMachineInstallCommand = (dir: string, uri: string) => {
 
 describe("integration tests for cli", () => {
     afterAll(async () => {
-        await rmAll(LOCAL_BASE_TEST_DIR)
-        await rmAll(REMOTE_BASE_TEST_DIR)
+        await rmAll(locations.LOCAL_BASE_TEST_DIR)
+        await rmAll(locations.REMOTE_BASE_TEST_DIR)
     })
 
     /*
@@ -173,8 +123,8 @@ describe("integration tests for cli", () => {
         local installs remote's machine creating a stored_machine
     */
     it("should bundle a flash drive, publish it, install it, create a machine, and install the machine", async () => {
-        await setup(localTestEnvironment, CARTI_TEST_LOCAL)
-        await setup(remoteTestEnvironment, CARTI_TEST_LOCAL)
+        await testUtil.installTestCarti(localTestEnvironment)
+        await testUtil.installTestCarti(remoteTestEnvironment)
 
         const localBundleCmd = testBundleCommand(localTestEnvironment.cwd)
         const publishBundleCmd = testPublishCommand(localTestEnvironment.cwd,
--- src/test/test_util.ts
+++ src/test/test_util.ts
@@ -1,5 +1,8 @@
 import { spawnSync, spawn } from "child_process"
-import { Readable } from "stream";
+import { Readable } from "stream"
+import fs from "fs-extra"
+import os from "os"
+const version = require("../../package.json").version; // tslint:disable-line
 
 export interface TestCommand {
     command:Array<string>
@@ -65,4 +68,90 @@ export async function testCommand(cmd: TestCommand, env: TestEnv, local: boolean
 export function shellCommand(cmd:string, env:TestEnv){
     const [command, ...args] = cmd.split(" ");
     return spawnSync(command, args, env)
+}
+
+export interface CartiTestLocations {
+    LOCAL_BASE_TEST_DIR: string
+    LOCAL_TEST_HOME: string
+    LOCAL_TEST_PROJECT_DIR: string
+    REMOTE_BASE_TEST_DIR: string
+    REMOTE_TEST_HOME: string
+    REMOTE_TEST_PROJECT_DIR: string
+} 
+
+export const createCartiTestLocations = (): CartiTestLocations => {
+    const LOCAL_BASE_TEST_DIR = fs.mkdtempSync(`${os.tmpdir()}/test-carti`)
+    const LOCAL_TEST_HOME = LOCAL_BASE_TEST_DIR
+    const LOCAL_TEST_PROJECT_DIR = `${LOCAL_BASE_TEST_DIR}/local-project`
+
+    const REMOTE_BASE_TEST_DIR = fs.mkdtempSync(`${os.tmpdir()}/test-carti`)
+    const REMOTE_TEST_HOME = REMOTE_BASE_TEST_DIR
+    const REMOTE_TEST_PROJECT_DIR = `${REMOTE_BASE_TEST_DIR}/remote-project`
+    return {
+        LOCAL_BASE_TEST_DIR,
+        LOCAL_TEST_PROJECT_DIR,
+        LOCAL_TEST_HOME,
+        REMOTE_BASE_TEST_DIR,
+        REMOTE_TEST_HOME,
+        REMOTE_TEST_PROJECT_DIR
+    }
+}
+export interface TestEnvironments {
+    remoteTestEnvironment: TestEnv
+    localTestEnvironment: TestEnv
+}
+
+export const cartiCmd = (pth: string) => `${pth}/node_modules/.bin/carti`
+
+export function isLocalScript(){
+    return process.env["CARTI_TEST_LOCAL"] ? true : false;
+}
+
+export const createTestEnvironments = (locations: CartiTestLocations): TestEnvironments =>{
+
+    const localTestEnvironment = createTestEnvironment(locations.LOCAL_TEST_PROJECT_DIR, 
+        locations.LOCAL_TEST_HOME, 
+        isLocalScript())
+    const remoteTestEnvironment = createTestEnvironment(locations.REMOTE_TEST_PROJECT_DIR, 
+        locations.REMOTE_TEST_HOME, 
+        isLocalScript())
+        return {
+            localTestEnvironment,
+            remoteTestEnvironment
+        }
+}
+
+// Takes project directory to build, test home directory, and then if local specified it sets the 
+// node_path to be the cwd for testing purposes
+export const createTestEnvironment = (projectDir: string, testHome: string, local: boolean): TestEnv => {
+    fs.ensureDirSync(projectDir)
+    let env: any = { HOME: testHome, PATH: process.env.PATH }
+    if (local)
+        env["NODE_PATH"] = `${process.cwd()}/node_modules`
+    return {
+        cwd: projectDir,
+        env
+    }
+}
+export const installTestCarti = async (env: TestEnv) => {
+    // setup environment to install itself in a clean dir
+    // set a static package directory to skip npm pack
+    if (!isLocalScript()) {
+        spawnSync("npm", ["pack"])
+        const cartiNodePackage = `createdreamtech-carti-${version}.tgz`
+        fs.copyFileSync(`${process.cwd()}/${cartiNodePackage}`, `${env.cwd}/${cartiNodePackage}`)
+        spawnSync("npm", ["init", "-y"], { cwd: env.cwd })
+        const res = spawnSync("npm", ["install", `${env.cwd}/${cartiNodePackage}`], { cwd: env.cwd })
+        if (res.error)
+            console.error(res.error)
+    }
+    fs.copyFileSync(`${__dirname}/../fixtures/dapp-test-data.ext2`, `${env.cwd}/dapp-test-data.ext2`)
+    const helpCommand = createTestCommand(`${cartiCmd(env.cwd)} --help`, contains("help"))
+    await testCommand(helpCommand, env, isLocalScript())
+}
+
+export const contains = (phrase: string) => {
+    return (res: string): boolean => {
+        return res.match(phrase) !== null
+    }
 }
